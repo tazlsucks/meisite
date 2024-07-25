b5aa2b9920b086e996fb3150cc49b0cfe463fc19
tazl <82299965+tazlsucks@users.noreply.github.com>
2024-07-24 23:18:21 -0400

    ugh

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 particlesJS('particles-js', {
         "particles": {
           "number": {
-            "value": 160,
+            "value": 300,
             "density": {
               "enable": true,
               "value_area": 800
